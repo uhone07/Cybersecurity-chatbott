ba48eb712616ff82e135345ede53805c4f7a281a
uhone07 <Uhonerambau100@gmail.com>
2026-05-26 14:36:13 +0200

    Refactor: Remove Program.cs entry point
    Removed the `Program` class and `Main` method, eliminating the application's entry point. Removed unused `using` directives for `Cybersecurity_chatbot.UI` and `System`. Added a placeholder comment indicating the entry point will be redefined in part 2.

--- Cybersecurity-chatbot/Program.cs
+++ Cybersecurity-chatbot/Program.cs
@@ -1,15 +1,1 @@
-﻿using Cybersecurity_chatbot.UI;
-using System;
-
-namespace CybersecurityChatbot
-{
-   
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            ConsoleUI ui = new ConsoleUI();
-            ui.Start();
-        }
-    }
-}
+﻿//main entry point of the application for part 2 
